3afe42a03114f32d918d5db032795b9f41aefbb8
scottyTwoskip <scottyeansor@gmail.com>
2023-08-24 16:02:06 -0700

    finished activity part 3 properly

--- views/places/index.jsx
+++ views/places/index.jsx
@@ -1,15 +1,24 @@
 const React = require('react')
-const Def = require('../Default')
+const Def = require('../default')
 
-function index () {
+function index (data) {
+  let placesFormatted = data.places.map((place) => {
     return (
-      <Def>
-          <main>
-              <h1>PLACES INDEX</h1>
-          </main>
-      </Def>
+      <div>
+        <h2>{place.name}</h2>
+        <img src={place.pic} alt={place.name}/>
+      </div>
     )
-  }
-  
+  })
+  return (
+    <Def>
+        <main>
+            <h1>PLACES INDEX PAGE</h1>
+            {placesFormatted}
+        </main>
+    </Def>
+)
+}
+
 
 module.exports = index 
