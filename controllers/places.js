ab26b26b83a2f10ba29c51d188308467ee5eac69
scottyTwoskip <scottyeansor@gmail.com>
2023-08-30 10:12:23 -0600

    added new places route(page)

--- controllers/places.js
+++ controllers/places.js
@@ -17,5 +17,12 @@ router.get('/', (req, res) => {
       
     res.render('places/index', {places})
 })
+router.get('/new', (req, res)=>{
+  res.render("places/new")
+})
+router.get('/:id', (req, res)=>{
+  res.send('hi')
+  // res.render("places/new")
+})
 
 module.exports = router
